94e32049feee588219b43fc3409ae024560b97cb
ibrahim-softsuave <ibrahim.softsuae@gmail.com>
2024-03-02 21:24:36 +0530

    adding coverage

--- src/components/hello.test.js
+++ src/components/hello.test.js
@@ -1,7 +1,7 @@
 import {render,screen} from '@testing-library/react'
 import Hello from '../components/hello'
 
-test('render given name',()=>{
+test('render given by name',()=>{
 render(<Hello name="ibrahim"/>)
 let ele = screen.getByText(/hello, ibrahim/i)
 expect(ele).toBeInTheDocument()
